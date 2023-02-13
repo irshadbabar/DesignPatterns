9a5aa70b010df4d96cf327400cc3d6cf8ff6bbfd
Irshad.Babar <Irshad.Babar@bentley.com>
2023-02-14 00:02:22 +0500

    adding Memento

---
+++ Memento/Originator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memento
+{
+    class Originator
+    {
+        public string State { get; set; }
+
+        public void SetState(string state)
+        {
+            Console.WriteLine("Originator: Setting state to " + state);
+            this.State = state;
+        }
+
+        public Memento SaveState()
+        {
+            Console.WriteLine("Originator: Saving to Memento.");
+            return new Memento(State);
+        }
+
+        public void RestoreState(Memento memento)
+        {
+            this.State = memento.State;
+            Console.WriteLine("Originator: State after restoring from Memento: " + State);
+        }
+    }
+}
---
+++ Memento/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Originator originator = new Originator();
+            originator.SetState("State1");
+            originator.SetState("State2");
+            Caretaker caretaker = new Caretaker();
+            caretaker.Memento = originator.SaveState();
+            originator.SetState("State3");
+            originator.RestoreState(caretaker.Memento);
+            Console.ReadKey();
+        }
+    }
+}
